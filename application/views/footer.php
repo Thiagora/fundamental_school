7eaa0a794e7907e7d645351b534b1a0b2e1d3c6a
Thiagora <thiagoalves1979@hotmail.com>
2018-12-08 14:20:55 -0500

    Enviando para TCC

---
+++ application/views/footer.php
@@ -0,0 +1,4 @@
+    <div style="clear:both;color:#aaa; padding:20px;">
+    	
+                        		<center>&copy; 2018, Fundamental School &copy;</center>
+                              </div>
